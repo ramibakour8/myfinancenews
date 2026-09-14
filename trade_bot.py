009845cab383e3e13f988dbe17533a6f8ec438ac
ramibakour8 <ramibakour8@gmail.com>
2026-09-14 07:56:59 +0300

    Update trade_bot.py

--- trade_bot.py
+++ trade_bot.py
@@ -1,3 +1,17 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Trade Bot is running", 200
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+Thread(target=run_web, daemon=True).start()
 import os
 import re
 import asyncio
